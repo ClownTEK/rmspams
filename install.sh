3d12025148d8a20f077a358bd21985cca24e4845
Giovanni Bechis <giovanni@paclan.it>
2016-06-25 00:10:56 +0200

    save updated ip address on disk

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ install -m 755 rmspams /usr/local/sbin
 cat <<EOF
 You should add this entry in your crontab(5)
 0 0 * * * /sbin/pfctl -t blacksmtp -T expire 86400
+10 0 * * * /sbin/pfctl -t blacksmtp -T show >/etc/blacksmtp
 to remove from blacklist old ip addresses.
 EOF
